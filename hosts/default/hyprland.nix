bbf83b799d2da80a829a0e25ac8c90af87d70c84
Finn Milner <finn.milner@outlook.com>
2025-04-25 20:01:57 +0100

    fix

--- hosts/default/hyprland.nix
+++ hosts/default/hyprland.nix
@@ -14,11 +14,12 @@
     withUWSM = true;
   };
 
-  wayland.windowManager.hyprland = {
-    enable = true;
+  # Needs to be under `home-manager.users.<username>`
+  # wayland.windowManager.hyprland = {
+  #   enable = true;
 
-    # Fix conflict with Home Manager
-    # https://wiki.hyprland.org/Useful-Utilities/Systemd-start/#uwsm
-    systemd.enable = false;
-  };
+  #   # Fix conflict with Home Manager
+  #   # https://wiki.hyprland.org/Useful-Utilities/Systemd-start/#uwsm
+  #   systemd.enable = false;
+  # };
 }
