46095ba687c61ecbc97a8a83d8fcf3253110e105
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-08-15 12:17:05 +0100

    fix

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -28,34 +28,4 @@ in
     (builtins.removeAttrs passwords [ "options" ])
   ];
   # === Imports ===
-}{
-  configRoot,
-  lib,
-  pkgs,
-  ...
-} @ args:
-
-let
-  # Get relative config position
-  configRelative = args.configRelative.security;
-
-  # Create args for child-modules
-  childArgs = { inherit configRoot configRelative lib pkgs; };
-
-  # Import child-modules
-  passwords = (import ./passwords childArgs);
-in
-{
-  # === Options ===
-  options = {
-    passwords = passwords.options;
-  };
-  # === Options ===
-
-
-  # === Imports ===
-  imports = [
-    (builtins.removeAttrs passwords [ "options" ])
-  ];
-  # === Imports ===
 }
