eca71495720f32040c1e03d18cc711275fb05beb
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-08-18 16:26:12 +0100

    feat: add to `lib.custom`

--- lib/default.nix
+++ lib/default.nix
@@ -9,4 +9,12 @@
 
   # Convert string to path relative to root
   fromRoot = lib.path.append ../.;  # Change this if this file moves
+
+  # Return the first non-null value in a list
+  firstNonNull = (vals: (
+    builtins.head (builtins.filter
+      (val: val != null)
+      (vals)
+    )
+  ))
 }
